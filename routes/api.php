760c4fe1c91bb2efa5bddaee866804ecb6c508ef
neto-mwg <neto@macwingo.com.br>
2023-09-22 09:10:46 -0300

    create routes feedbacks API

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,22 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Feedback\FeedbackController;
 use App\Http\Controllers\Api\SupportController;
-use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-// Route::post('/login', [AuthController::class, 'auth']);
+Route::post('/login', [AuthController::class, 'auth']);
+Route::post('/user/create', [AuthController::class, 'new']);
 
-Route::apiResource('/users', UserController::class);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::apiResource('/supports', SupportController::class);
+    Route::get('/feedback', [FeedbackController::class, 'index']);
+    Route::post('/feedback/create', [FeedbackController::class, 'store']);
+
+    Route::apiResource('/supports', SupportController::class);
+});
+
+// Route::apiResource('/users', UserController::class);
+
